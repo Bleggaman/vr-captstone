049dcf8232054316cd01acff5fbfe3fb52489fcf
Britt Henderson <Bleggaman@yahoo.com>
2019-10-09 11:28:08 -0700

    inital layout established

---
+++ hendeb96-vr-project/Assets/Scripts/RotateOnView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnView : MonoBehaviour {
+
+    public Transform subject;
+    public float lowerWindow;
+    public float upperWindow;
+    private bool rotated = false;
+    
+    // Update is called once per frame
+    void Update() {
+        if (!this.rotated && this.InView()) {
+            this.rotated = true;
+            Debug.Log("Rotated open");
+        } else if (this.rotated && !this.InView()) {
+            this.rotated = false;
+            Debug.Log("Rotated closed");
+        }
+    }
+
+
+    private bool InView() {
+        return this.subject.rotation.eulerAngles.y > lowerWindow && this.subject.rotation.eulerAngles.y < upperWindow;
+    }
+}
