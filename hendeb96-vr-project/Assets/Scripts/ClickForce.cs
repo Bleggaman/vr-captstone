ad02c6b262743031975efd66f52e5e4ad46b263c
Britt Henderson <Bleggaman@yahoo.com>
2019-10-08 17:29:08 -0700

    trying out initial scene

---
+++ hendeb96-vr-project/Assets/Scripts/ClickForce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ClickForce : MonoBehaviour {
+    public float strength;
+    public float rotateStrength;
+    
+    void Update() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            var forward = this.transform.rotation * Vector3.forward;
+            var click = Camera.main.ScreenPointToRay(Input.mousePosition);
+//            Debug.Log("mouse at " + click);
+//            Ray beam = new Ray(this.transform.position, forward);
+            Debug.DrawRay(click.origin, click.direction, Color.magenta);
+            RaycastHit hit;
+            //Debug.DrawRay(this.transform.position, forward, Color.red);
+            if (Physics.Raycast(click, out hit)) {
+                Debug.Log("Hit the " + hit.collider.name);
+                var rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+                if (rb != null) {
+                    rb.AddForce(Vector3.up * strength);
+                    rb.AddTorque(forward * rotateStrength);
+                }
+            }
+        }
+    }
+}
