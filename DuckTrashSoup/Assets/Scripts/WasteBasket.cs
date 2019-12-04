3f6eeb209ed93e643286ed154353ea775e7d45af
Britt Henderson <Bleggaman@yahoo.com>
2019-12-04 11:55:15 -0800

    added player controller, paper, and space jam music on throwing paper into bin. Testing now

---
+++ DuckTrashSoup/Assets/Scripts/WasteBasket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasteBasket : MonoBehaviour {
+
+    public AudioSource spaceJam;
+    
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("paperWad")) return;
+        
+        // TODO: throw confetti
+        
+        this.spaceJam.Play();
+    }
+}
