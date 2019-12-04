0816683301a4cd7927af4409f71144dab4512fb0
Britt Henderson <Bleggaman@yahoo.com>
2019-12-04 14:18:08 -0800

    added multipke wods of paper

--- DuckTrashSoup/Assets/Scripts/StackOfPaper.cs
+++ DuckTrashSoup/Assets/Scripts/StackOfPaper.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StackOfPaper : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class StackOfPaper : MonoBehaviour {
+
+    public static StackOfPaper Instance;
+    
+    public GameObject paperPrefab;
+    public int numWods;
+
+    private List<GameObject> paperWods;
+    
+    
+    void Awake() {
+        Instance = this;
+        paperWods = new List<GameObject>();
+        for (int i = 0; i < numWods; i++) {
+            var wod = Instantiate(paperPrefab, this.transform.position, Quaternion.identity);
+            paperWods.Add(wod);
+            for (int j = 0; j < i; j++) {
+                Physics.IgnoreCollision(wod.GetComponent<Collider>(), paperWods[j].GetComponent<Collider>());
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
         
     }
 }
