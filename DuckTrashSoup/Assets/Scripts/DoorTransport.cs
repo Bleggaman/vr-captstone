f37f3ad6308e6bd936110b3bfc3d62811c161b08
Britt Henderson <Bleggaman@yahoo.com>
2019-12-04 15:45:07 -0800

    added door trasnport. testing

---
+++ DuckTrashSoup/Assets/Scripts/DoorTransport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTransport : OVRGrabbable {
+
+    public string targetRoom;
+    public MeshRenderer door;
+    public Material lit;
+    public Material unlit;
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint) {
+        SceneManager.LoadScene(targetRoom);
+    }
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) return;
+
+        this.door.material = lit;
+    }
+
+    public void OnTriggerExit(Collider other) {
+        if (other.CompareTag("Player")) return;
+
+        this.door.material = unlit;
+    }
+}
