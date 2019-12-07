2f06129c8612a94967c9c934356ce8ed064fe2e8
yerimh <yerimh@uw.edu>
2019-12-06 23:21:49 -0800

    Added some scripts

---
+++ DuckTrashSoup/Assets/Scripts/DuckQuack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckQuack : MonoBehaviour
+{
+    public AudioSource quack;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        quack.Play();
+
+        if (other.gameObject.tag == "Player")
+        {
+            Physics.IgnoreCollision(this.GetComponent<Collider>(), other.collider);
+        }
+    } 
+    
+}
